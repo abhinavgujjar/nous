4c69425e3c5df9775fa20c92ef44b58152ea3015
abhinav <abhinavgujjar@gmail.com>
2014-06-27 15:18:15 +0530

    initial commit

---
+++ angular-seed-master/app/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/listing', {templateUrl: 'partials/listing.html', controller: 'MyCtrl1'});
+  $routeProvider.otherwise({redirectTo: '/main'});
+}]);
